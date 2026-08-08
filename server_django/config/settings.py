62cad41950616a6998a6a1c01baf9fbc7e136f97
Dax Adorno <daxcoins4@gmail.com>
2026-08-08 00:29:04 -0300

    chore: configurando  backend dando caldad a las herramientas

--- server_django/config/settings.py
+++ server_django/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-+n*b0l45yzhqyub4$ytnp6pkys8-sp$$t_865r%vu7rg#tc+wi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: list[str] = []
 
 
 # Application definition
--- server_django/tests/test_healthcheck.py
+++ server_django/tests/test_healthcheck.py
@@ -1,4 +1,4 @@
-﻿import pytest
+import pytest
 from django.urls import reverse
 from rest_framework.test import APIClient
 
